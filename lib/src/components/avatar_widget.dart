0bfcb4d578ecf852357797d2eb4048b4e080e192
Wooing <32007781+wooing1084@users.noreply.github.com>
2022-04-06 20:08:23 +0900

    main feed

--- lib/src/components/avatar_widget.dart
+++ lib/src/components/avatar_widget.dart
@@ -2,7 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 //아바타(프로필) 종류가 여러가지임
-enum AvatarType { TYPE1, TYPE2, TYPE3}
+enum AvatarType { TYPE1, TYPE2, TYPE3 }
 
 class AvatarWidget extends StatelessWidget {
   bool? hasStory;
@@ -11,66 +11,76 @@ class AvatarWidget extends StatelessWidget {
   AvatarType type;
   double? size;
 
-  AvatarWidget({Key? key,
-    required this.type,
-    required this.thumbPath,
-    this.name,
-    this.hasStory,
-    this.size = 65
-  }) : super(key: key);
+  AvatarWidget(
+      {Key? key,
+      required this.type,
+      required this.thumbPath,
+      this.name,
+      this.hasStory,
+      this.size = 65})
+      : super(key: key);
 
-  Widget type1Widget()
-  {
+  Widget type1Widget() {
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topRight, end: Alignment.bottomLeft,
-              colors: [Colors.purple, Colors.orange]),
-      shape: BoxShape.circle,
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.purple, Colors.orange]),
+        shape: BoxShape.circle,
       ),
-      width: size,height: size, margin: EdgeInsets.symmetric(horizontal: 5),
+      width: size, height: size, margin: EdgeInsets.symmetric(horizontal: 5),
       padding: EdgeInsets.all(2),
       //cahedNetworkImage는 인터넷상의 다운로드 가능한 이미지 url을 이용하여 이미지를 출력시키는것을 말한다.
       //이 기능을 사용하기 위해선 터미널에 add를 해주어야한다
       child: Container(
-        padding: EdgeInsets.all(2),
-        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-          child: ClipRRect(borderRadius: BorderRadius.circular(size!),
-            child: CachedNetworkImage(imageUrl: thumbPath,fit: BoxFit.cover,)
-          )
-      ),
+          padding: EdgeInsets.all(2),
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(size!),
+              child: CachedNetworkImage(
+                imageUrl: thumbPath,
+                fit: BoxFit.cover,
+              ))),
     );
   }
 
-  Widget type2Widget()
-  {
+  Widget type2Widget() {
     return Container(
-      width: size, height: size,
+        width: size,
+        height: size,
         padding: EdgeInsets.all(2),
         decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-        child: ClipRRect(borderRadius: BorderRadius.circular(size!),
-            child: CachedNetworkImage(imageUrl: thumbPath,fit: BoxFit.cover,)
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(size!),
+            child: CachedNetworkImage(
+              imageUrl: thumbPath,
+              fit: BoxFit.cover,
+            )));
+  }
+
+  Widget type3Widget() {
+    return Row(
+      children: [
+        type1Widget(),
+        Text(
+          name ?? '',
+          style: TextStyle(color: Colors.black, fontSize: 15),
         )
+      ],
     );
   }
 
-  Widget type3Widget()
-  {
-    return Row(
-      children: [type1Widget(),
-      Text(name??'', style: TextStyle(color: Colors.black, fontSize: 20),)],
-    );
-  }
   @override
   Widget build(BuildContext context) {
-    switch(type)
-    {
+    switch (type) {
       case AvatarType.TYPE1:
         return type1Widget();
       case AvatarType.TYPE2:
         return type2Widget();
-        case AvatarType.TYPE3:
-          return type3Widget();
+      case AvatarType.TYPE3:
+        return type3Widget();
         break;
     }
 
--- lib/src/components/post_Widget.dart
+++ lib/src/components/post_Widget.dart
@@ -1,43 +1,123 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutproj/src/components/avatar_widget.dart';
+import 'package:flutproj/src/components/imageData.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PostWidget extends StatelessWidget {
   const PostWidget({Key? key}) : super(key: key);
 
-  Widget _header()
-  {
+  Widget _header() {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        AvatarWidget(type: AvatarType.TYPE3,name: 'Guest_Name',size: 45,
-            thumbPath: 'https://pds.joins.com/news/component/htmlphoto_mmdata/201506/04/htm_20150604145312c010c011.jpg'),
+        AvatarWidget(
+            type: AvatarType.TYPE3,
+            name: 'Guest_Name',
+            size: 45,
+            thumbPath:
+                'https://pds.joins.com/news/component/htmlphoto_mmdata/201506/04/htm_20150604145312c010c011.jpg'),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: GestureDetector(
+            onTap: () {},
+            child: ImageData(
+              IconsPath.postMoreIcon,
+              width: 40,
+            ),
+          ),
+        ),
       ],
     );
   }
 
-  Widget _image()
-  {
+//이미지 컨트롤러 만듦(업로드 이미지 갯수)
+  Widget _image() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 3.0),
+      alignment: Alignment.topLeft,
+      child: CachedNetworkImage(
+        imageUrl:
+            'https://t1.daumcdn.net/movie/0007909f71efba0cfe508d68bb479f7592367730',
+        width: Get.width,
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+
+  Widget _info() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ImageData(
+                    IconsPath.likeOffIcon,
+                    width: 60,
+                  ),
+                ),
+              ),
+              SizedBox(width: 15),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ImageData(
+                    IconsPath.replyIcon,
+                    width: 60,
+                  ),
+                ),
+              ),
+              SizedBox(width: 15),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ImageData(
+                    IconsPath.directMessage,
+                    width: 60,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {},
+              child: ImageData(
+                IconsPath.bookMarkOffIcon,
+                width: 60,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _descript() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          '좋아요 999개',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Text('예시 컨텐츠입니다.\n예시 컨텐츠입니다.\n예시 컨텐츠입니다.\n예시 컨텐츠입니다.')
+      ],
+    );
+  }
+
+  Widget _reply() {
     return Container();
   }
 
-  Widget _info()
-  {
+  Widget _dateAgo() {
     return Container();
   }
-  Widget _descript()
-  {
-    return Container();
-  }
-
-  Widget _reply()
-  {
-    return Container();
-  }
-
-  Widget _dateAgo()
-  {
-    return Container();
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,11 +126,11 @@ class PostWidget extends StatelessWidget {
       child: Column(
         children: [
           _header(),
-         // _image(),
-         // _info(),
-         // _descript(),
-         // _reply(),
-         // _dateAgo()
+          _image(),
+          _info(),
+          _descript(),
+          // _reply(),
+          // _dateAgo()
         ],
       ),
 //      decoration: BoxDecoration(border: Border.all(width: 1,color: Colors.black)),
